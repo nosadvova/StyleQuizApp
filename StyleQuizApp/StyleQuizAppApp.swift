b3734e28a723ccc70cb1d872b7104f9fe30c65c4
Volodymyr Novosad <vovanovosad2003@gmail.com>
2025-07-18 14:07:44 +0300

    Initial Commit

---
+++ StyleQuizApp/StyleQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleQuizAppApp.swift
+//  StyleQuizApp
+//
+//  Created by Vova Novosad on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct StyleQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
